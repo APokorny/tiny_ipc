320c19006390100ad9c12920081dfff8fc4a5369
Andreas Pokorny <andreas.pokorny@siemens.com>
2021-10-01 20:08:44 +0200

    cleanup example

--- example/chat.h
+++ example/chat.h
@@ -11,8 +11,10 @@ namespace chat
 namespace ti = tiny_ipc;
 using namespace ti::literals;
 
-constexpr auto chat      = ti::protocol(ti::method<bool(::ucred)>("connect"_m), ti::method<void(std::string)>("send"_m),
-                              ti::signal<void(std::string)>("text_added"_s));
+constexpr auto chat = ti::protocol(           //
+    ti::method<bool(::ucred)>("connect"_m),   //
+    ti::method<void(std::string)>("send"_m),  //
+    ti::signal<void(std::string)>("text_added"_s));
 using chat_protocol = std::remove_const_t<std::decay_t<decltype(chat)>>;
 }  // namespace chat
 
